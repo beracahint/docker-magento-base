3a132cce9b74ea6bf401943993ea08f5322b0249
Edgar Gonzalez <edgar.gonzalez@teradata.com>
2018-02-13 00:50:44 -0800

    AWS Codebuild spec file

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,9 @@
 #   EMEDIC_MAGE_FS_BASE_S3_URL
 #
 #*********************************************************
-MAGE_FILESYSTEM_S3_URL="s3://emedic360-build-artifacts/magento-filesystem/magento2.2.2-48f3935f-8b1e-4a6e-8f4f-7a4f0578f961.tar.gz"
+#MAGE_FILESYSTEM_S3_URL="s3://emedic360-build-artifacts/magento-filesystem/magento2.2.2-48f3935f-8b1e-4a6e-8f4f-7a4f0578f961.tar.gz"
+
+echo $MAGE_FILESYSTEM_S3_URL
 
 if [ -z $MAGE_FILESYSTEM_S3_URL ]; then
 	echo -e  "[ERROR] MAGE_FILESYSTEM_S3_URL env variable is mandatory"
@@ -16,8 +18,7 @@ fi
 rm -rf mage_filesystem
 mkdir mage_filesystem
 
-aws s3 cp $MAGE_FILESYSTEM_S3_URL mage_filesystem/
+aws s3 cp $MAGE_FILESYSTEM_S3_URL mage_filesystem/filesystem.tar.gz
 
 
-
-#docker build -t emedic/magento-base:1.0.0 .
+docker build -t emedic/magento-base:1.0.0 .
