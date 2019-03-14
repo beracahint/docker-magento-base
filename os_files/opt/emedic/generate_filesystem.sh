c8304f4962e07bc7426a8a05316201f79853f672
Jose Luis Lopez Valenzuela <jluis@iMac-de-Jose.local>
2019-03-14 16:46:05 -0700

    generate filesystem corregido_2

--- os_files/opt/emedic/generate_filesystem.sh
+++ os_files/opt/emedic/generate_filesystem.sh
@@ -6,5 +6,5 @@ cd /var/www/html
 tar --exclude "./var/cache" --exclude "./var/page_cache" --exclude "./pub/static" --exclude "./pub/var/cache" --exclude "./pub/var/composer_home" --exclude "./pub/var/generation" --exclude "./pub/var/page_cache" --exclude "./pub/var/view_preprocessed" --exclude './pub/media/catalog/product' -cf ../$FILE_NAME .
 tar -rf ../$FILE_NAME pub/static/.htaccess
 gzip ../$FILE_NAME
-aws s3 cp ../${FILE_NAME}.gz s3://beracah-cicd/magento-filesystem/${FILE_NAME}.gz
+aws s3 cp ../${FILE_NAME}.gz s3://beracah-cicd/${FILE_NAME}.gz
 rm ../${FILE_NAME}.gz
